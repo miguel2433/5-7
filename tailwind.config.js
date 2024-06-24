298a2c882fd0d7d83b7072e98e0725acce2bedf5
miguel2433 <miguelitocap12@gmail.com>
2024-06-24 18:21:47 -0300

    uitggjkgj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
+  darkMode: 'selector',
   content: ["./MainProyect/**/*.{html, js}"],
   theme: {
-    darkMode: 'selector',
+   
     extend: {
       colors: {
         green: "#6C987E",
         white: "#ffff",
+        negro: "#100D14",
+        darkLigt: "56586B",
+        darkGrow: "424457",
+        gris: "#D4D4D4",
+        negrito: "#1A1520",
+        verde: "##668d76"
       }
     },
   },
