648ee338c80259841d7bba13d7f0bf9df1cb7b5c
miguel2433 <miguelitocap12@gmail.com>
2024-07-26 03:53:36 -0300

    ale

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,19 @@ module.exports = {
   darkMode: 'selector',
   content: ["./MainProyect/**/*.{html, js}"],
   theme: {
-   
     extend: {
       colors: {
         green: "#6C987E",
-        white: "#ffff",
+        white: "#FFFF",
         negro: "#100D14",
-        darkLigt: "56586B",
-        darkGrow: "424457",
+        fondoClaro: "#333548",
+        darkGrow: "#424457",
         gris: "#D4D4D4",
         negrito: "#1A1520",
-        verde: "#668d76"
+        verde: "#668D76",
+        grisLetra: "#707070",
+        verdeLetra: "#508466",
+        verdeClaro: "#96B19A"
       }
     },
   },
